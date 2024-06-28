7a0623bc227af4d595adb3d76517b799891d3bf3
seilj <been22026@gmail.com>
2024-06-28 11:25:19 +0900

    aaa

---
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -0,0 +1,48 @@
+package com.example.myapplication
+
+import android.graphics.drawable.Drawable
+import android.os.Bundle
+import com.google.android.material.bottomnavigation.BottomNavigationView
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.ContextCompat
+import androidx.fragment.app.Fragment
+import androidx.navigation.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.setupActionBarWithNavController
+import androidx.navigation.ui.setupWithNavController
+import com.example.myapplication.databinding.ActivityMainBinding
+import com.google.android.material.tabs.TabLayoutMediator
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        initView()
+    }
+
+    private fun initView() {
+        val viewPager = binding.viewpager
+        val tabLayout = binding.tabLayout
+
+        val fragmentList = ArrayList<Fragment>()
+        fragmentList.add(Fragment1())
+        fragmentList.add(Fragment2())
+        fragmentList.add(Fragment3())
+
+        viewPager.adapter = ViewPagerAdapter(fragmentList, this)
+
+        val iconList = ArrayList<Drawable?>()
+        iconList.add(ContextCompat.getDrawable(this, R.drawable.ic_launcher_foreground))
+        iconList.add(ContextCompat.getDrawable(this, R.drawable.ic_launcher_foreground))
+        iconList.add(ContextCompat.getDrawable(this, R.drawable.ic_launcher_foreground))
+
+        TabLayoutMediator(tabLayout, viewPager) { tab, position ->
+            tab.text = "${position}번"
+            tab.icon = iconList[position]
+        }.attach()
+    }
+}
---
+++ app/src/main/java/com/example/myapplication/ViewPagerAdapter.kt
@@ -0,0 +1,13 @@
+package com.example.myapplication
+
+import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.Fragment
+import androidx.viewpager2.adapter.FragmentStateAdapter
+
+class ViewPagerAdapter(
+    private val fragmentList: ArrayList<Fragment>,
+    container: AppCompatActivity
+): FragmentStateAdapter(container.supportFragmentManager, container.lifecycle) {
+    override fun getItemCount(): Int = fragmentList.count()
+    override fun createFragment(position: Int): Fragment = fragmentList[position]
+}
