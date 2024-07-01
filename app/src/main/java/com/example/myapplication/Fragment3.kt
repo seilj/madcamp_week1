97ed6b4a295c25ab61861b91e3105918d1c90697
seilj <been22026@gmail.com>
2024-07-01 15:25:53 +0900

    7.1-2

--- app/src/main/java/com/example/myapplication/Fragment3.kt
+++ app/src/main/java/com/example/myapplication/Fragment3.kt
@@ -1,27 +1,40 @@
 package com.example.myapplication
 
+import android.R
 import android.os.Build
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ArrayAdapter
+import android.widget.Button
+import android.widget.CalendarView
+import android.widget.Spinner
 import android.widget.TextView
 import androidx.annotation.RequiresApi
 import androidx.fragment.app.Fragment
 import com.example.myapplication.databinding.Fragment3Binding
-import java.time.DayOfWeek
 import java.time.LocalDate
 import java.time.YearMonth
+import java.time.format.DateTimeFormatter
 
-
-class Fragment3: Fragment() {
+data class Student(val name: String,val regularClass: MutableList<LocalDate>, val offDays: MutableList<LocalDate>, val makeupDays: MutableList<Pair<LocalDate, Int>>)
+class Fragment3 : Fragment() {
     private lateinit var binding: Fragment3Binding
-    //private val days = arrayListOf("Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday")
-    private val offDays = mutableListOf<LocalDate>()
-    private val makeupDays = mutableListOf<LocalDate>()
-    private val hourly_wage = 30000
-    private val class_hour = 4
+    private val students = mutableListOf<Student>()
     private lateinit var totalSalaryTextView: TextView
+    private val peopleList: List<PeopleData>
+        get() {
+            val mainActivity = activity as? MainActivity
+            return mainActivity?.getPeopleList() ?: emptyList()
+        }
+    private var selectedDate: LocalDate? = null
+    private var studentsOnSelectedDate: List<Student> = emptyList()
+    private lateinit var calendarView: CalendarView
+    private lateinit var spinner: Spinner
+    private lateinit var cancleBtn: Button
+    private lateinit var makeupBtn: Button
+    private lateinit var deleteBtn: Button
 
     @RequiresApi(Build.VERSION_CODES.O)
     override fun onCreateView(
@@ -30,85 +43,89 @@ class Fragment3: Fragment() {
         savedInstanceState: Bundle?
     ): View {
         binding = Fragment3Binding.inflate(inflater, container, false)
-        val calendarView = binding.calendarView
-        val cancleBtn = binding.cancleBtn
-        val makeupBtn = binding.makeupBtn
-        val deleteBtn = binding.deleteBtn
+        calendarView = binding.calendarView
+        spinner = binding.spinner
+        cancleBtn = binding.cancleBtn
+        makeupBtn = binding.makeupBtn
+        deleteBtn = binding.deleteBtn
         totalSalaryTextView = binding.totalSalary
 
-        calendarView.setOnDateChangeListener{view, year, month, dayOfMonth ->
-            val selectedDate = LocalDate.of(year, month+1, dayOfMonth)
-            val dayOfWeek = selectedDate.dayOfWeek
+        listInitialize()
 
-            if(dayOfWeek == DayOfWeek.SUNDAY){
-                if(offDays.contains(selectedDate)){
-                    cancleBtn.visibility = View.INVISIBLE
-                    makeupBtn.visibility = View.INVISIBLE
-                    deleteBtn.visibility = View.VISIBLE
-                    deleteBtn.setOnClickListener {
-                        offDays.remove(selectedDate)
-                    }
-                }
-                else{
-                    cancleBtn.visibility = View.VISIBLE
-                    makeupBtn.visibility = View.INVISIBLE
-                    deleteBtn.visibility = View.INVISIBLE
-                    cancleBtn.setOnClickListener{
-                        offDays.add(selectedDate)
-                    }
-                }
-            }
-            else{
-                if(makeupDays.contains(selectedDate)){
-                    cancleBtn.visibility = View.INVISIBLE
-                    makeupBtn.visibility = View.INVISIBLE
-                    deleteBtn.visibility = View.VISIBLE
-                    deleteBtn.setOnClickListener {
-                        makeupDays.remove(selectedDate)
-                    }
-                }
-                else{
-                    cancleBtn.visibility = View.INVISIBLE
-                    makeupBtn.visibility = View.VISIBLE
-                    deleteBtn.visibility = View.INVISIBLE
-                    makeupBtn.setOnClickListener{
-                        makeupDays.add(selectedDate)
-                    }
-                }
-            }
-
-            updatePayment()
+        calendarView.setOnDateChangeListener { _, year, month, dayOfMonth ->
+            selectedDate = LocalDate.of(year, month + 1, dayOfMonth)
+            updateStudentsOnSelectedDate()
         }
+
+        cancleBtn.setOnClickListener {
+            val selectedStudentName = spinner.selectedItem as? String
+            selectedStudentName?.let { name ->
+                val student = students.find { it.name == name }
+                selectedDate?.let { date ->
+                    student?.let {
+                        it.offDays.add(date)
+                        updateStudentsOnSelectedDate()
+                        updatePayment()
+                    }
+                }
+            }
+        }
+
         updatePayment()
 
         return binding.root
     }
 
     @RequiresApi(Build.VERSION_CODES.O)
-    private fun updatePayment(){
-        val yearMonth = YearMonth.now()
-        val totalSalary = calculateMonthlyPayment(yearMonth, hourly_wage, class_hour, offDays, makeupDays)
+    private fun listInitialize() {
+        for (person in peopleList) {
+            val regularClass = mutableListOf<LocalDate>()
+            val yearMonth = YearMonth.now()
+            val daysInMonth = yearMonth.lengthOfMonth()
+            for (day in 1..daysInMonth) {
+                val date = LocalDate.of(yearMonth.year, yearMonth.monthValue, day)
+                val formatter = DateTimeFormatter.ofPattern("EEEE")
+                val dayOfWeekString = date.format(formatter)
+                if (person.week == dayOfWeekString)
+                    regularClass.add(date)
+            }
+            students.add(Student(person.name, regularClass, mutableListOf(), mutableListOf()))
+        }
+    }
+
+    @RequiresApi(Build.VERSION_CODES.O)
+    private fun updateStudentsOnSelectedDate() {
+        selectedDate?.let { date ->
+            studentsOnSelectedDate = students.filter {
+                it.regularClass.contains(date) && !it.offDays.contains(date)
+            }
+            if (studentsOnSelectedDate.isNotEmpty()) {
+                spinner.visibility = View.VISIBLE
+                cancleBtn.visibility = View.VISIBLE
+                val studentNames = studentsOnSelectedDate.map { it.name }
+                spinner.adapter = ArrayAdapter(requireContext(), android.R.layout.simple_spinner_dropdown_item, studentNames)
+            } else {
+                spinner.visibility = View.GONE
+                cancleBtn.visibility = View.GONE
+            }
+        }
+    }
+
+    @RequiresApi(Build.VERSION_CODES.O)
+    private fun updatePayment() {
+        val totalSalary = calculateMonthlyPayment()
         totalSalaryTextView.text = "이번 달 과외비: ${totalSalary}원"
     }
 
     @RequiresApi(Build.VERSION_CODES.O)
-    private fun calculateMonthlyPayment(
-        yearMonth: YearMonth,
-        hourlyWage: Int,
-        classHour: Int,
-        offDays: List<LocalDate>,
-        makeupDays: List<LocalDate>
-    ): Int {
-        var totalPayment = 0
-        val daysInMonth = yearMonth.lengthOfMonth()
-        for (day in 1..daysInMonth) {
-            val date = LocalDate.of(yearMonth.year, yearMonth.monthValue, day)
-            if (date.dayOfWeek == DayOfWeek.SUNDAY && !offDays.contains(date)) {
-                totalPayment += hourlyWage * classHour
-            }
+    private fun calculateMonthlyPayment(): Int {
+        var totalPayment: Double = 0.0
+        for (student in students) {
+            val person = peopleList.find { it.name == student.name }
+            val hourlyWage = person?.hourlyWage ?: 0.0
+            val hours = person?.hourPerNumber ?: 0.0
+            totalPayment += (student.regularClass.size - student.offDays.size).toDouble() * hours * hourlyWage * 10000
         }
-        totalPayment += hourlyWage * classHour * makeupDays.size
-
-        return totalPayment
+        return totalPayment.toInt()
     }
 }
